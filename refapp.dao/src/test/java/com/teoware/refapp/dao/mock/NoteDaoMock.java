67336517e4a6ee6845d316022d689bcd26de8655
Teo <onlyteo@live.com>
2012-04-03 18:25:37 +0000

    Update of DAO EJBs
    git-svn-id: http://svn.dev.teoware.com/svn/refapp/trunk@17 ef5b14da-52b2-446c-88e8-5ba10cdcf0bf

---
+++ refapp.dao/src/test/java/com/teoware/refapp/dao/mock/NoteDaoMock.java
@@ -0,0 +1,17 @@
+package com.teoware.refapp.dao.mock;
+
+import com.teoware.refapp.dao.NoteDaoLocal;
+import com.teoware.refapp.dto.NoteCreateRequest;
+import com.teoware.refapp.dto.NoteCreateResponse;
+
+public class NoteDaoMock implements NoteDaoLocal {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public NoteCreateResponse createNote(NoteCreateRequest noteCreateRequest) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ refapp.dao/src/test/java/com/teoware/refapp/dao/mock/TaskDaoMock.java
@@ -0,0 +1,16 @@
+package com.teoware.refapp.dao.mock;
+
+import com.teoware.refapp.dao.TaskDaoLocal;
+import com.teoware.refapp.dto.TaskCreateRequest;
+import com.teoware.refapp.dto.TaskCreateResponse;
+
+public class TaskDaoMock implements TaskDaoLocal {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public TaskCreateResponse createTask(TaskCreateRequest taskCreateRequest) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
