807c0e19b720d8aa8f60793841e09fb62a849b1d
Teo <onlyteo@live.com>
2012-04-03 18:35:28 +0000

    Updated web presentation layer.
    git-svn-id: http://svn.dev.teoware.com/svn/refapp/trunk@23 ef5b14da-52b2-446c-88e8-5ba10cdcf0bf

--- refapp.web/src/main/java/com/teoware/refapp/web/presentation/IndexPageBean.java
+++ refapp.web/src/main/java/com/teoware/refapp/web/presentation/IndexPageBean.java
@@ -2,25 +2,19 @@ package com.teoware.refapp.web.presentation;
 
 import java.io.Serializable;
 
-import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.RequestScoped;
 
-import com.teoware.refapp.service.AuthorService;
-
-@ManagedBean
-@SessionScoped
+@ManagedBean(name="hello")
+@RequestScoped
 public class IndexPageBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
-	@EJB(mappedName="AuthorService")
-	private AuthorService authorService;
-	
 	private String name;
 
 	public String getName() {
-		return authorService.getName();
+		return name;
 	}
 
 	public void setName(String name) {
