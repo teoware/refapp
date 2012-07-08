96d0d067399e6e0abb7f6cb19f6c12d506f16827
Teo <onlyteo@live.com>
2012-07-08 13:59:31 +0000

    Added front page JSF bean.
    git-svn-id: http://svn.dev.teoware.com/svn/refapp/trunk@35 ef5b14da-52b2-446c-88e8-5ba10cdcf0bf

---
+++ code/refapp.web.ui/src/main/java/com/teoware/refapp/web/ui/frontpage/FrontPageBean.java
@@ -0,0 +1,23 @@
+package com.teoware.refapp.web.ui.frontpage;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name="hello")
+@SessionScoped
+public class FrontPageBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
