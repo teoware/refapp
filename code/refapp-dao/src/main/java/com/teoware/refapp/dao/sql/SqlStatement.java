4bd1089019c110d1992649bb6b7a6ddc1bcb3ac0
Teo <onlyteo@live.com>
2012-10-15 17:40:02 +0200

    Fix for last commit of SqlStatement.

--- code/refapp-dao/src/main/java/com/teoware/refapp/dao/sql/SqlStatement.java
+++ code/refapp-dao/src/main/java/com/teoware/refapp/dao/sql/SqlStatement.java
@@ -12,101 +12,106 @@ public class SqlStatement {
 		return sql;
 	}
 
+	public void append(String append) {
+		sql += append;
+	}
+
 	public String build() {
-	    return sql;
+		return sql;
 	}
 
 	public class Builder {
 
-	    private StringBuffer sql;
+		private StringBuffer sql;
 
-	    public Builder() {
-	        sql = new StringBuffer();
-	    }
+		public Builder() {
+			sql = new StringBuffer();
+		}
 
-	    public Builder doInsert(String tableName) {
-	        sql.append("INSERT INTO " + tableName);
-	        return this;
-	    }
+		public Builder doInsert(String tableName) {
+			sql.append("INSERT INTO " + tableName);
+			return this;
+		}
 
-	    public Builder withColumns(String... columns) {
-	        sql.append(" (" + join(columns, ", ") + ")");
-	        return this;
-	    }
+		public Builder withColumns(String... columns) {
+			sql.append(" (" + join(columns, ", ") + ")");
+			sql.append(withValues(columns.length));
+			return this;
+		}
 
-	    private Builder withValues(int length) {
-	        String values = "?";
-	        for (int i = 1; i < length; i++) {
-                values += ", ?";
-            }
-	        sql.append(" VALUES (" + values + ")");
-	        return this;
-	    }
+		private Builder withValues(int length) {
+			String values = "?";
+			for (int i = 1; i < length; i++) {
+				values += ", ?";
+			}
+			sql.append(" VALUES (" + values + ")");
+			return this;
+		}
 
-	    public Builder doUpdate(String tableName) {
-	        sql.append("UPDATE " + tableName);
-	        return this;
-	    }
+		public Builder doUpdate(String tableName) {
+			sql.append("UPDATE " + tableName);
+			return this;
+		}
 
-	    public Builder setColumnsTo(String... columns) {
-	        sql.append(" SET (" + join(append(columns, " = ?"), ", ") + ")");
-	        return this;
-	    }
+		public Builder setColumnsTo(String... columns) {
+			sql.append(" SET (" + join(append(columns, " = ?"), ", ") + ")");
+			return this;
+		}
 
-	    public Builder doSelect(String... columns) {
-	        sql.append("SELECT (" + join(columns, ", ") + ")");
-	        return this;
-	    }
+		public Builder doSelect(String... columns) {
+			sql.append("SELECT (" + join(columns, ", ") + ")");
+			return this;
+		}
 
-	    public Builder from(String table) {
-	        sql.append(" FROM " + table);
-	        return this;
-	    }
+		public Builder from(String table) {
+			sql.append(" FROM " + table);
+			return this;
+		}
 
-	    public Builder where(String column) {
-	        sql.append(" WHERE " + column + " = ?");
-	        return this;
-	    }
+		public Builder where(String column) {
+			sql.append(" WHERE " + column + " = ?");
+			return this;
+		}
 
-	    public Builder where(String... column) {
-	        sql.append(" WHERE (" + join(append(column, " = ?"), " AND ") + ")");
-	        return this;
-	    }
+		public Builder where(String... column) {
+			sql.append(" WHERE (" + join(append(column, " = ?"), " AND ") + ")");
+			return this;
+		}
 
-	    public Builder and(String column) {
-	        sql.append(" AND " + column + " = ?");
-	        return this;
-	    }
+		public Builder and(String column) {
+			sql.append(" AND " + column + " = ?");
+			return this;
+		}
 
-	    public Builder and(String... column) {
-	        sql.append(" AND (" + join(append(column, " = ?"), " AND ") + ")");
-	        return this;
-	    }
+		public Builder and(String... column) {
+			sql.append(" AND (" + join(append(column, " = ?"), " AND ") + ")");
+			return this;
+		}
 
-	    public Builder or(String column) {
-	        sql.append(" OR " + column + " = ?");
-	        return this;
-	    }
+		public Builder or(String column) {
+			sql.append(" OR " + column + " = ?");
+			return this;
+		}
 
-	    public Builder or(String... column) {
-	        sql.append(" OR (" + join(append(column, " = ?"), " OR ") + ")");
-	        return this;
-	    }
+		public Builder or(String... column) {
+			sql.append(" OR (" + join(append(column, " = ?"), " OR ") + ")");
+			return this;
+		}
 
-        private String[] append(String[] strings, String append) {
-            for (int i = 0; i < strings.length; i++) {
-                strings[i] = strings[1] + append;
-            }
-            return strings;
-        }
+		private String[] append(String[] strings, String append) {
+			for (int i = 0; i < strings.length; i++) {
+				strings[i] = strings[1] + append;
+			}
+			return strings;
+		}
 
-	    private String join(String[] strings, String join) {
-	        int i = 0;
-	        String string = strings[i++];
-	        for (; i < strings.length; i++) {
-                string += join + strings[i];
-            }
-	        return string;
-	    }
+		private String join(String[] strings, String join) {
+			int i = 0;
+			String string = strings[i++];
+			for (; i < strings.length; i++) {
+				string += join + strings[i];
+			}
+			return string;
+		}
 	}
 }
