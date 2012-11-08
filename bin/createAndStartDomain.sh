fab2c727778e7fce91191bb6a3bf5ad8a050dfc8
Teo <onlyteo@live.com>
2012-11-08 21:38:42 +0100

    Updated scripts.

--- bin/createAndStartDomain.sh
+++ bin/createAndStartDomain.sh
@@ -4,4 +4,6 @@ FILE_PATH="$(dirname "$(readlink -f ${BASH_SOURCE[0]})")"
 
 ${FILE_PATH}/createDomain.sh
 
+${FILE_PATH}/createJDBCResource.sh
+
 ${FILE_PATH}/startDomain.sh
--- bin/stopAndDeleteDomain.sh
+++ bin/stopAndDeleteDomain.sh
@@ -4,4 +4,6 @@ FILE_PATH="$(dirname "$(readlink -f ${BASH_SOURCE[0]})")"
 
 ${FILE_PATH}/stopDomain.sh
 
+${FILE_PATH}/deleteJDBCResource.sh
+
 ${FILE_PATH}/deleteDomain.sh
