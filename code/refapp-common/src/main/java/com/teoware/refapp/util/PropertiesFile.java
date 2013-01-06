d262f2dd66a14fb67bd3ffa67856c376b4b6f6e2
Teo <onlyteo@live.com>
2013-01-06 17:07:26 +0100

    Added functionality to properties file to process embedded variable keys.

--- code/refapp-common/src/main/java/com/teoware/refapp/util/PropertiesFile.java
+++ code/refapp-common/src/main/java/com/teoware/refapp/util/PropertiesFile.java
@@ -6,23 +6,48 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Properties;
+import java.util.Set;
 
 public class PropertiesFile extends Properties {
 
 	private static final long serialVersionUID = 1L;
 
-	public void loadFromFile(String fileName) throws FileNotFoundException, IOException {
+	public void loadFromFile(String fileName) throws IOException {
 		super.load(new FileInputStream(fileName));
+		prosessProperties();
 	}
 
 	public void loadFromClasspath(String fileName) throws IOException {
 		super.load(this.getClass().getResourceAsStream(fileName));
+		prosessProperties();
 	}
 
 	public void loadUsingClassLoader(String fileName) throws IOException {
-		URL url =  ClassLoader.getSystemResource(fileName);
-		if (url != null) {
-			super.load(new FileInputStream(new File(url.getFile())));
+		URL url = ClassLoader.getSystemResource(fileName);
+		if (url == null) {
+			throw new FileNotFoundException("File '" + fileName + "' could not be found.");
+		}
+		super.load(new FileInputStream(new File(url.getFile())));
+		prosessProperties();
+	}
+
+	private void prosessProperties() {
+		Set<Object> keySet = super.keySet();
+		Object[] keys = keySet.toArray();
+		for (int i = 0; i < keys.length; i++) {
+			Object key1 = keys[i];
+			Object value1 = super.get(key1);
+			String var = "${" + key1 + "}";
+			loop2: for (int j = 0; j < keys.length; j++) {
+				Object key2 = keys[j];
+				String value2 = super.get(key2).toString();
+				if (value2.contains(var)) {
+					super.remove(key2);
+					super.put(key2, value2.replace(var, value1.toString()));
+					i = 0;
+					break loop2;
+				}
+			}
 		}
 	}
 
@@ -34,20 +59,44 @@ public class PropertiesFile extends Properties {
 		return super.getProperty(key, defaultValue);
 	}
 
-	public int getInt(String key) {
+	public Integer getInteger(String key) {
 		return new Integer(super.getProperty(key));
 	}
 
-	public int getInt(String key, int defaultValue) {
-		return new Integer(super.getProperty(key, new Integer(defaultValue).toString()));
+	public Integer getInteger(String key, Integer defaultValue) {
+		return new Integer(super.getProperty(key, defaultValue.toString()));
 	}
 
-	public boolean getBoolean(String key) {
+	public Long getLong(String key) {
+		return new Long(super.getProperty(key));
+	}
+
+	public Long getLong(String key, Long defaultValue) {
+		return new Long(super.getProperty(key, defaultValue.toString()));
+	}
+
+	public Float getFloat(String key) {
+		return new Float(super.getProperty(key));
+	}
+
+	public Float getFloat(String key, Float defaultValue) {
+		return new Float(super.getProperty(key, defaultValue.toString()));
+	}
+
+	public Double getDouble(String key) {
+		return new Double(super.getProperty(key));
+	}
+
+	public Double getDouble(String key, Double defaultValue) {
+		return new Double(super.getProperty(key, defaultValue.toString()));
+	}
+
+	public Boolean getBoolean(String key) {
 		return new Boolean(super.getProperty(key));
 	}
 
-	public boolean getBoolean(String key, boolean defaultValue) {
-		return new Boolean(super.getProperty(key, new Boolean(defaultValue).toString()));
+	public Boolean getBoolean(String key, Boolean defaultValue) {
+		return new Boolean(super.getProperty(key, defaultValue.toString()));
 	}
 
 	public static PropertiesFile createFromFile(String fileName) throws FileNotFoundException, IOException {
--- code/refapp-common/src/test/java/com/teoware/refapp/util/PropertiesFileTest.java
+++ code/refapp-common/src/test/java/com/teoware/refapp/util/PropertiesFileTest.java
@@ -1,45 +1,112 @@
 package com.teoware.refapp.util;
 
+import static java.io.File.separator;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
-import com.teoware.refapp.util.PropertiesFile;
-
 @Category(com.teoware.refapp.test.UnitTestGroup.class)
 public class PropertiesFileTest {
 
+	private static PropertiesFile properties;
+
 	@BeforeClass
-	public static void oneTimeSetUp() {
-		
-	}
-
-	@AfterClass
-	public static void oneTimeTearDown() {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
+	public static void oneTimeSetUp() throws IOException {
+		properties = PropertiesFile.createFromClasspath("/properties/test.properties");
 	}
 
 	@Test
-	public void testLoadPropertiesFileFromClasspath() throws IOException {
-		PropertiesFile properties = PropertiesFile.createFromClasspath("/properties/test.properties");
-		String value = properties.get("testKey");
-		
+	public void testCreateFromFile() throws IOException {
+		assertNotNull(PropertiesFile.createFromFile(
+				"src" + separator + "test" + separator + "resources" + separator + "properties" + separator
+						+ "test.properties").get("stringKey"));
+	}
+	
+	@Test(expected = FileNotFoundException.class)
+	public void testCreateFromFileNoFileFound() throws IOException {
+		assertNotNull(PropertiesFile.createFromFile(
+				"src" + separator + "test" + separator + "resources" + separator + "properties" + separator
+						+ "notfound.properties").get("stringKey"));
+	}
+
+	@Test
+	public void testCreateFromClasspath() throws IOException {
+		assertNotNull(PropertiesFile.createFromClasspath("/properties/test.properties").get("stringKey"));
+	}
+
+	@Test
+	public void testCreateUsingClassLoader() throws IOException {
+		// TODO assertNotNull(PropertiesFile.createUsingClassLoader("test.properties").get("stringKey"));
+	}
+
+	@Test
+	public void testStringValue() {
+		String value = properties.get("stringKey");
+
 		assertNotNull(value);
 		assertEquals("testValue", value);
+	}
+
+	@Test
+	public void testIntegerValue() {
+		int value = properties.getInteger("intKey");
+
+		assertNotNull(value);
+		assertEquals(7, value);
+	}
+
+	@Test
+	public void testLongValue() {
+		long value = properties.getLong("longKey");
+
+		assertNotNull(value);
+		assertEquals(8L, value);
+	}
+
+	@Test
+	public void testFloatValue() {
+		float value = properties.getFloat("floatKey");
+
+		assertNotNull(value);
+		assertEquals(0.7F, value, 0.01);
+	}
+
+	@Test
+	public void testDoubleValue() {
+		double value = properties.getDouble("doubleKey");
+
+		assertNotNull(value);
+		assertEquals(0.8D, value, 0.01);
+	}
+
+	@Test
+	public void testBooleanValue() {
+		boolean value = properties.getBoolean("booleanKey");
+
+		assertNotNull(value);
+		assertTrue(value);
+	}
+
+	@Test
+	public void testVar1Value() {
+		String value = properties.get("varKey1");
+
+		assertNotNull(value);
+		assertEquals("hey there", value);
+	}
+
+	@Test
+	public void testVar2Value() {
+		String value = properties.get("varKey2");
+
+		assertNotNull(value);
+		assertEquals("hey there mr.", value);
 	}
 }
